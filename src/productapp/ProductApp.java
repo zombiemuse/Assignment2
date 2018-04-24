268c648bbfdea928c6f665ece89dd8cf03c0385e
zombiemuse <xxzombiemusexx@yahoo.com>
2018-04-23 18:57:02 -0700

    Update ProductApp.java

--- src/productapp/ProductApp.java
+++ src/productapp/ProductApp.java
@@ -5,6 +5,8 @@
  */
 package productapp;
 
+import java.util.Scanner;
+
 /**
  *
  * @author xxzom
@@ -15,8 +17,44 @@ public class ProductApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        ReadProduct list = new ReadProduct();
-        list.readProductFile();
+        System.out.println("Type 'code' to search by product code, or type 'list' to list inventory (Type 'quit' to end): ");
+        Scanner sc = new Scanner(System.in);
+        
+        outer:
+        while (true){
+            System.out.print("> ");
+            String command = sc.nextLine();
+            command = command.toLowerCase();
+            
+            switch(command){
+                case "code":
+                    ReadProduct codeList = new ReadProduct();
+                    codeList.getMapCodeProduct();
+                    System.out.print("Product code: ");
+                    Scanner code = new Scanner(System.in);
+                    
+                   
+                    break;
+                
+                case "list":
+                    ReadProduct list = new ReadProduct();
+                    list.readProductFile();
+                    
+                    
+                    break;
+                   
+                case "quit":
+                    break outer;
+                    
+                default:
+                    System.out.println(command + " is not a valid option!\nTry again!");
+                    break;
+            }
+        }
+        
+        
+//        ReadProduct list = new ReadProduct();
+//        list.readProductFile();
         
         
         
